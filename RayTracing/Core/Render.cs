57289fedd28b5b9b1eb17d9eef379304f1b69ca9
Proniaiev Maksym <m.pronyayev@beter.co>
2023-08-12 15:40:17 +0300

    simple ppm file

---
+++ RayTracing/Core/Render.cs
@@ -0,0 +1,36 @@
+﻿
+namespace RayTracing.Core
+{
+    public static class Render
+    {
+        public const int ImageWidth = 256;
+        public const int ImageHeight = 256;
+        public const double ToIConversionFactor = 255.999;
+        private const string ImageName = "./image.ppm";
+        
+        public static async Task RenderSimpleImage()
+        {
+            await using var stream = new StreamWriter(ImageName);
+            await stream.WriteLineAsync("P3");
+            await stream.WriteLineAsync(ImageWidth + " " + ImageHeight);
+            await stream.WriteLineAsync("255");
+            for (int i = 0; i < ImageHeight; ++i)
+            {
+                for (int j = 0; j < ImageWidth; ++j)
+                {
+                    Console.WriteLine("\rScanlines remaining: " + (ImageHeight - j));
+
+                    var red = (double)i / (ImageWidth - 1);
+                    var green = (double)j / (ImageHeight - 1);
+                    var blue = 0;
+
+                    var ired = (int) (ToIConversionFactor * red);
+                    var igreen = (int) (ToIConversionFactor * green);
+                    var iblue = (int) (ToIConversionFactor * blue);
+                    await stream.WriteLineAsync($"{ired} {igreen} {iblue}");
+                }
+            }
+            Console.WriteLine("Done! Access on: " + Path.GetFullPath(ImageName));
+        }
+    }
+}
---
+++ RayTracing/Startup.cs
@@ -0,0 +1,11 @@
+﻿using RayTracing.Core;
+
+namespace RayTracing;
+
+public class Startup
+{
+    public static async Task Main(string[] args)
+    {
+        await Render.RenderSimpleImage();
+    }
+}
